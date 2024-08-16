2526de293d53a5199197ec07b025c6fa865b1039
shubham.tandale <shubham.tandale@paymentz.com>
2024-08-16 17:19:32 +0530

    Initial Commit

---
+++ StandardCheckoutKiDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  StandardCheckoutKiDemo
+//
+//  Created by Vinicius on 09/06/17.
+//  Copyright © 2017 Paymentz. All rights reserved.
+//
+
+import UIKit
+import StandardCheckoutKit
+
+class ViewController: UIViewController, StandardCheckoutDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+  
+    @IBAction func pay(_ sender: UIButton) {
+        
+        let secureKey = "bzI93aEQeYDeE50Pa929NiDk3us8XTbU"
+        
+        let requestParameters = RequestParameters()
+        requestParameters.memberId = "10558"
+        requestParameters.paymentMode = PaymentMode.CC
+        requestParameters.terminalId = "7079"
+        requestParameters.merchantTransactionId = "randomnumber265757"
+        requestParameters.amount = "50.00"
+        requestParameters.currency = "USD"
+        requestParameters.toType = "paymentz"
+        requestParameters.paymentBrand = PaymentBrand.visa
+        requestParameters.orderDescription = "Test"
+        requestParameters.country = "IN"
+        requestParameters.state = "MH"
+        requestParameters.street = "Malad"
+        requestParameters.city = "Mumbai"
+        requestParameters.email = "savitha.m@paymentz.com"
+        requestParameters.postCode = "400064"
+        requestParameters.telnocc = "+91"
+        requestParameters.phone = "9096831666"
+        requestParameters.hostUrl = "https://sandbox.paymentplug.com/transaction/Checkout"
+        requestParameters.device = "ios"
+        let standardCheckout = StandardCheckout(viewController: self)
+        standardCheckout.initPayment(requestParameters: requestParameters, standardCheckoutDelegate: self, secureKey: secureKey)
+        
+    }
+
+    // Delegate methods is where you will receive the payment result
+    func onSuccess(standardCheckoutResult: StandardCheckoutResult) {
+        let resultViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        let json = standardCheckoutResult.json
+        resultViewController.standardCheckoutResult = "\(json)"
+        let resultNavigationController: UINavigationController = UINavigationController(rootViewController: resultViewController)
+        self.present(resultNavigationController, animated: true, completion: nil)
+    }
+    
+    func onFail(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
