e24f6a5dce4efebd4c265ed527e5966ba1e63867
shubham.tandale <shubham.tandale@paymentz.com>
2024-09-04 14:15:06 +0530

    Request changes, url changed to test.paymentz.com

--- StandardCheckoutKiDemo/ViewController.swift
+++ StandardCheckoutKiDemo/ViewController.swift
@@ -38,27 +38,27 @@ class ViewController: UIViewController, StandardCheckoutDelegate {
             return
         }
 
-        let secureKey = "bzI93aEQeYDeE50Pa929NiDk3us8XTbU"
+        let secureKey = "YOiHKCvM2XSHcMEMbBrHCYzNS8iIZOi4"
         
         let requestParameters = RequestParameters()
-        requestParameters.memberId = "10558"
+        requestParameters.memberId = "13219"
         requestParameters.paymentMode = ""
-        requestParameters.terminalId = "7079"
+        requestParameters.paymentBrand = ""
+        requestParameters.terminalId = ""
         requestParameters.merchantTransactionId = merchantId
         requestParameters.amount = String(amount)
-        requestParameters.currency = "USD"
+        requestParameters.currency = "INR"
         requestParameters.toType = "paymentz"
-        requestParameters.paymentBrand = ""
         requestParameters.orderDescription = "Test"
         requestParameters.country = "IN"
         requestParameters.state = "MH"
         requestParameters.street = "Malad"
         requestParameters.city = "Mumbai"
-        requestParameters.email = "savitha.m@paymentz.com"
+        requestParameters.email = "tejas.gandhi@paymentz.com"
         requestParameters.postCode = "400064"
         requestParameters.telnocc = "+91"
         requestParameters.phone = "9096831666"
-        requestParameters.hostUrl = "https://sandbox.paymentplug.com/transaction/Checkout"
+        requestParameters.hostUrl = "https://test.paymentz.com/transaction/Checkout"
         requestParameters.device = "ios"
         let standardCheckout = StandardCheckout(viewController: self)
         standardCheckout.initPayment(requestParameters: requestParameters, standardCheckoutDelegate: self, secureKey: secureKey)
@@ -68,16 +68,20 @@ class ViewController: UIViewController, StandardCheckoutDelegate {
     // Delegate methods is where you will receive the payment result
     func onSuccess(standardCheckoutResult: StandardCheckoutResult) {
         print("standardCheckoutResult: \(standardCheckoutResult.json)")
-        let resultCode = standardCheckoutResult.json["resultCode"] as? Int ?? 0
+        let resultCode = Int(standardCheckoutResult.json["resultCode"] as? String ?? "0")
         let message = standardCheckoutResult.json["resultDescription"] as? String
         
         if resultCode == 00001{
             let alert = UIAlertController(title: "Success", message: message, preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default){_ in
+                self.navigationController?.popViewController(animated: true)
+            })
             self.present(alert, animated: true, completion: nil)
         }else{
             let alert = UIAlertController(title: "Failed", message: message, preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default){_ in
+                self.navigationController?.popViewController(animated: true)
+            })
             self.present(alert, animated: true, completion: nil)
         }
 
